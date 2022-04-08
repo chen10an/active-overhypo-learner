279e972f26e3c3ce81a93d58aa8d104b93fde968
Chentian Jiang <chentian.jiang@gmail.com>
2022-04-08 15:34:25 +0100

    make conditioner return joint EIG

--- src/main/scala/activeOverhypoLearner/Conditioner.scala
+++ src/main/scala/activeOverhypoLearner/Conditioner.scala
@@ -4,13 +4,13 @@ import utils._
 object Conditioner {
   // Condition on a fixed set of events [intervention and outcome] (meant for participant intervention data) and output intermediate metrics
 
-  def getPhaseResults(learner: PhaseLearner, events: Vector[Event]): (Array[(Array[Double], Array[Double], Double, Double, Double, Map[Fform, Double], Map[Set[Block], Double], Map[Hyp, Double])], Array[Set[Block]], Dist[Hyp]) = {
+  def getPhaseResults(learner: PhaseLearner, events: Vector[Event]): (Array[(Array[Double], Array[Double], Array[Double], Double, Double, Double, Map[Fform, Double], Map[Set[Block], Double], Map[Hyp, Double])], Array[Set[Block]], Dist[Hyp]) = {
     // returns:
     // intermediate metrics of sequentially conditioning `learner` on each event
     // possible interventions in the same order as metric arrays (so that it's possible to match possible intervention to metric value)
     // final updated learner's hypsDist: can be used to initialize a Learner with whatever mixins --> call transfer to get a new learner with the same mixins --> put back into getPhaseResults (for conditioning the next transfer phase)
 
-    var results = Array.ofDim[(Array[Double], Array[Double], Double, Double, Double, Map[Fform, Double], Map[Set[Block], Double], Map[Hyp, Double])](events.length)
+    var results = Array.ofDim[(Array[Double], Array[Double], Array[Double], Double, Double, Double, Map[Fform, Double], Map[Set[Block], Double], Map[Hyp, Double])](events.length)
 
     var ithLearner = learner
     val possibleInterventions:Array[Set[Block]] = learner.allInterventions.toArray  // fixed order
@@ -21,6 +21,7 @@ object Conditioner {
 
       val comboToFformEIG = ithLearner.fformComboValMap
       val comboToStructEIG = ithLearner.structComboValMap
+      val comboToJointEIG = ithLearner.comboValMap
 
       // only save the max atoms if not more than 10
       val emptyFform = Fform("NA", (n: Int) => -1.0)
@@ -35,6 +36,7 @@ object Conditioner {
       results(i) = (
         possibleInterventions.map(comboToFformEIG(_)).toArray,
         possibleInterventions.map(comboToStructEIG(_)).toArray,
+        possibleInterventions.map(comboToJointEIG(_)).toArray,
         ithLearner.priorFformMarginal.entropy,
         ithLearner.priorStructMarginal.entropy,
         ithLearner.hypsDist.entropy,
