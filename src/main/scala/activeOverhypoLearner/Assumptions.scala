ad36f280e63508e6fb9cb01fc13243899a3caec3
Chentian Jiang <chentian.jiang@gmail.com>
2022-04-12 04:54:00 +0100

    add 0.3 bin size

--- src/main/scala/activeOverhypoLearner/Assumptions.scala
+++ src/main/scala/activeOverhypoLearner/Assumptions.scala
@@ -32,3 +32,18 @@ trait point15FformBinSize extends PhaseLearner {
     new PhaseLearner(newJointDist) with point15FformBinSize
   }
 }
+
+trait point3FformBinSize extends PhaseLearner {
+
+  override val fformBinSize: Double = 0.3
+
+  override def update(events: Vector[Event]): PhaseLearner with point3FformBinSize = {
+    new PhaseLearner(multiPosterior(events)) with point3FformBinSize
+  }
+
+  override def transfer(allBlocks: Set[Block], currWeight: Double = 1.0, otherFformDist: Dist[Fform] = Dist(Map.empty[Fform, Double])): PhaseLearner with point3FformBinSize = {
+    val newJointDist: Dist[Hyp] = super.transfer(allBlocks, currWeight, otherFformDist).hypsDist
+
+    new PhaseLearner(newJointDist) with point3FformBinSize
+  }
+}
